44c9f59f6d343c51c65ae79ae871619d17bc1901
danial137 <danial79fakhrabadi@gmail.com>
2026-09-03 01:30:35 +0330

    feat: enhance Sidebar and DashboardWrapper for improved state management and UI responsiveness

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,17 +1,29 @@
 "use client"
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
 import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
+
+  const dispatch = useAppDispatch()
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  }
+
+  const sidebarClassNames = `fixed flex flex-col ${isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"} bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
   return (
-    <div>
-      <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+    <div className={sidebarClassNames}>
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
 
         <div>logo</div>
 
         <h1 className='font-extrabold text-2xl'>PersianStock</h1>
 
-        <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => { }}>
+        <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={toggleSidebar}>
 
           <Menu />
 
