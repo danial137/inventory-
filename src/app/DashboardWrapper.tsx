932b47de814562f629930a9ea20053c54fd8018f
danial137 <danial79fakhrabadi@gmail.com>
2026-09-03 00:32:28 +0330

    feat: integrate useAppSelector in DashboardWrapper for sidebar state management

--- src/app/DashboardWrapper.tsx
+++ src/app/DashboardWrapper.tsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import Navbar from "@/app/(components)/Navbar"
 import Sidebar from "@/app/(components)/Sidebar"
-import StoreProvider from './redux'
+import StoreProvider, { useAppSelector } from './redux'
 
 
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+
+    const isSidebarCollapsed = useAppSelector
+
     return (
-        <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-            <Sidebar />
-            <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}>
-                <Navbar />
-                {children}
-            </main>
-        </div>
+        <StoreProvider>
+            <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+                <Sidebar />
+                <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}>
+                    <Navbar />
+                    {children}
+                </main>
+            </div>
+        </StoreProvider>
     )
 }
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <StoreProvider>
+            <DashboardLayout>
+                {children}
+            </DashboardLayout>
 
-            {children}
         </StoreProvider>
     )
 }
