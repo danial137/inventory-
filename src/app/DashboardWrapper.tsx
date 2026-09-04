b7c4ac15c7510a95c5299143d4303b5f8d1a57c1
danial137 <danial79fakhrabadi@gmail.com>
2026-09-05 01:10:30 +0330

    feat: enhance Navbar, Sidebar, and DashboardWrapper components for improved dark mode support and UI responsiveness

--- src/app/DashboardWrapper.tsx
+++ src/app/DashboardWrapper.tsx
@@ -1,49 +1,49 @@
-"use client"
-import React, { useEffect } from 'react'
-import Navbar from "@/app/(components)/Navbar"
-import Sidebar from "@/app/(components)/Sidebar"
-import StoreProvider, { useAppSelector } from './redux'
+"use client";
 
+import React, { useEffect } from "react";
 
+import Navbar from "@/app/(components)/Navbar";
+
+import Sidebar from "@/app/(components)/Sidebar";
+
+import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-
-    const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+    );
 
     const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
-
     useEffect(() => {
-
-        if (isDarkMode) {
-            document.documentElement.classList.add("dark")
-        } else {
-            document.documentElement.classList.add("light")
-        }
+        document.body.classList.toggle("dark", isDarkMode);
+        document.documentElement.classList.toggle("dark", isDarkMode);
+    }, [isDarkMode]);
 
 
-    }, [])
 
     return (
-
-        <div className={` ${isDarkMode ? "dark" : "light"} light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+        <div
+            className="flex bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white w-full min-h-screen"
+        >
             <Sidebar />
-            <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-0"} `}>
+
+            <main
+                className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 dark:bg-gray-900 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+                    }`}
+            >
                 <Navbar />
                 {children}
             </main>
         </div>
+    );
+};
 
-    )
-}
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <StoreProvider>
-            <DashboardLayout>
-                {children}
-            </DashboardLayout>
-
+            <DashboardLayout>{children}</DashboardLayout>
         </StoreProvider>
-    )
-}
+    );
+};
 
-export default DashboardWrapper
+export default DashboardWrapper;
