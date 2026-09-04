efce28f84f2b4d49ef8755976c7f5a855d8beb3e
danial137 <danial79fakhrabadi@gmail.com>
2026-09-04 23:35:34 +0330

    feat: add sidebar link component with active state management and navigation

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -2,6 +2,8 @@
 import { useAppDispatch, useAppSelector } from '@/app/redux'
 import { setIsSidebarCollapsed } from '@/state'
 import { LucideIcon, Menu } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 interface SidebarLinkProps {
@@ -17,7 +19,22 @@ const sidebarLink = ({
   label,
   isCollapsed
 }: SidebarLinkProps) => {
-  
+  const pathname = usePathname();
+  const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+
+
+  return (
+    <Link href={href}>
+
+      <div className={`cursor-pointer flex items-center ${isCollapsed ? "justify-center py-4" : 'justify-start px-8 py-4'}
+      hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? "bg-blue-200 text-white" : ""}
+  }`}>
+
+      </div>
+
+
+    </Link>
+  )
 }
 
 const Sidebar = () => {
