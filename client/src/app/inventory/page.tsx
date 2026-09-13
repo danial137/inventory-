b6e28053eaec6cd24da6d0f6f92d2313eaf27fa1
danial137 <danial79fakhrabadi@gmail.com>
2026-09-14 01:11:44 +0330

    feat: add DataGrid component to display product inventory with enhanced column definitions

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -2,9 +2,17 @@
 
 import { useGetProductsQuery } from "@/state/api"
 import Header from "../(components)/Header";
+import { GridColDef, DataGrid } from "@mui/x-data-grid";
 
 
+const column: GridColDef[] = [
+    { field: "productId", headerName: "ID", width: 90 },
+    { field: "name", headerName: "Product Name", width: 200 },
+    { field: "price", headerName: "Price", width: 110, type: "number", valueGetter: (value, row) => `$${row.price}` },
+    { field: "rating", headerName: "Rating", width: 110, type: "number", valueGetter: (value, row) => row.rating ? row.rating : "N/A" },
+    { field: "stockQuantity", headerName: "Stock Quantity", width: 150, type: "number" }
 
+]
 
 const Inventory = () => {
 
@@ -24,9 +32,10 @@ const Inventory = () => {
 
     }
 
-    return <div className="flex flex-col">
+    return <div className="flex flex-col ">
 
         <Header name="Inventory" />
+        <DataGrid rows={products} columns={column} getRowId={(row) => row.productId} checkboxSelection />
 
     </div>
 
