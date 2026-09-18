c16cb944ad68201b4d6864e62e7f74d3cd20b4fc
danial137 <danial79fakhrabadi@gmail.com>
2026-09-18 22:18:05 +0330

    feat: enhance Products page with product details and rating display

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { useGetProductsQuery } from '@/state/api'
-import { SearchIcon } from 'lucide-react'
+import { PlusCircle, SearchIcon } from 'lucide-react'
 import { useState } from 'react'
 import Header from '../(components)/Header'
+import Rating from '../(components)/Rating'
 
 const Products = () => {
 
@@ -38,10 +39,53 @@ const Products = () => {
 
             <Header name='Products' />
 
-            <button className='flex items-center bg-blue hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded'>
+            <button className='flex items-center bg-blue hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded' onClick={() => setIsModalOpen(true)}>
+
+                <PlusCircle className='w-5 h-5 mr-2 text-gray-200!' />
 
 
             </button>
+
+        </div>
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 lg:grid-cols-3 gap-10 justify-between'>
+
+
+            {isLoading ? (<div>Loading...</div>) : (
+                products?.map((product) => (
+                    <div key={product.productId} className='border shadow rounded-md p-4 max-w-full w-ful mx:auto'>
+
+                        <div className='flex flex-col items-center'>
+
+                            img
+
+                            <h3 className='text-lg font-semibold'>
+                                {product.name}
+                            </h3>
+                            <p className='text-gray-8000'>${product.price.toFixed(2)}</p>
+                            <div className='text-sm  mt-1'>
+
+                                Stock:{product.stockQuantity}
+
+                            </div>
+
+                            {product.rating && (
+                                <div className='flex items-center mt-2'>
+
+                                    <Rating rating={product.rating} />
+
+                                </div>
+                            )}
+
+                        </div>
+
+
+
+                    </div>
+                ))
+            )}
+
+
 
         </div>
 
